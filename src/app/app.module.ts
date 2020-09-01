58e3b976f8b792a679449b670d828b8a7e13ddb8
Osdark <odvelasquez@unac.edu.co>
2020-09-01 18:23:54 -0500

    logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/date-picker/date-picker.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css']
+})
+export class DatePickerComponent {
+
+  mathBirthday = 0;
+  now = new Date();
+  diffDay = 0;
+  private MsPerDay = 1000*60*60*24;
+  private daysRemaining = 0;
+
+  constructor() { }
+
+  getDiffDay(date: MatDatepickerInputEvent<Date>) {
+    this.diffDay = Math.floor((this.now.getTime() - date.value.getTime())/this.MsPerDay);
+    this.mathBirthday = this.get10n(this.diffDay);
+    this.daysRemaining = this.mathBirthday - this.diffDay;
+  }
+
+  get10n(diffDay: number): number {
+    const exp = diffDay.toString().length;
+    return Math.pow(10, exp);
+  }
+
+  getMathBirthday(): Date {
+    const nextMathBirthdayDate = new Date();
+    nextMathBirthdayDate.setDate(this.now.getDate() + this.daysRemaining);
+    return nextMathBirthdayDate;
+  }
+
+}
